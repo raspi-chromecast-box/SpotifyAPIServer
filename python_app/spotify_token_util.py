ad13907872e97f520b63393c040c0b673223174e
raspi-chromecast-box <raspiccbox03@gmail.com>
2020-04-27 08:39:03 -0400

    81

--- python_app/spotify_token_util.py
+++ python_app/spotify_token_util.py
@@ -71,7 +71,9 @@ def get_spotify_token_info( redis_connection=None ):
 			redis_connection = redis_utils.try_to_connect_to_redis()
 		if redis_connection == False:
 			return False
+		print( "we have redis" )
 		spotify_token_info = RefreshSpotifyTokenIfNecessary( redis_connection )
+		print( spotify_token_info )
 		if spotify_token_info == False:
 			return False
 		return spotify_token_info
